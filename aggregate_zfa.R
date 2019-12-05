d87950d50a214866afbe8594573e3e21a23331a9
Richard White <richard.white@sanger.ac.uk>
2019-12-05 14:08:07 +0000

    Add options to aggregate_zfa.R
    aggregate_zfa.R: Add --log10p_col and --fe_col to allow for
    column names that differ from default.

--- aggregate_zfa.R
+++ aggregate_zfa.R
@@ -6,7 +6,11 @@ option_list <- list(
   make_option("--group_by_cols", type="character", action="store", default='Set,Parent.Term',
               help="Name of the columns to group by for aggregation [default %default]"),
   make_option("--filter_by_cols", type="character", action="store", default='Parent.Term',
-              help="Name of the columns to filter by [default %default]")
+              help="Name of the columns to filter by [default %default]"),
+  make_option("--log10p_col", type="character", action="store", default='log10p',
+              help="Name of the column for log10(pvalue) [default %default]"),
+  make_option("--fe_col", type="character", action="store", default='FE',
+              help="Name of the column for Fold Enrichment [default %default]")
 )
 
 desc <- paste('', 'Script to aggregate ZFA terms by another column',
@@ -29,6 +33,8 @@ group_by_cols <-
 filter_by_cols <-
         unlist( strsplit(cmd_line_args$options[['filter_by_cols']],
                           ',', fixed = TRUE) )
+log10p_col <- cmd_line_args$options[['log10p_col']]
+fe_col <- cmd_line_args$options[['fe_col']]
 
 packages <- c('tidyverse')
 for( package in packages ){
@@ -41,6 +47,10 @@ data <- read.delim(cmd_line_args$args[1])
 for (col in group_by_cols) {
     data[[col]] <- factor(data[[col]], levels = unique(data[[col]]))
 }
+
+# convert log10 and fe col names to expected col names
+colnames(data)[ colnames(data) == log10p_col ] <- 'log10p'
+colnames(data)[ colnames(data) == fe_col ] <- 'FE'
 
 aggregated_data <- data %>%
     filter_at(filter_by_cols, all_vars(!is.na(.))) %>%
