510734d3d6627138097cd0d4f81f3f5a785691cb
Richard White <rjw26@cam.ac.uk>
2020-10-29 15:44:45 +0000

    Changes to go_barchart.R
    Allow column names to be specified for y, fill variables and
    for which bars are plotted to the left and right (Up vs Down)

--- go_barchart.R
+++ go_barchart.R
@@ -1,12 +1,26 @@
+#!/usr/bin/env Rscript
+
 library('optparse')
 
 option_list <- list(
   make_option("--output_file", type="character", default='go_plot.pdf',
               help="Output file name [default %default]" ),
-  make_option("--RData_file", type="character", default='go_plot.rda',
-              help="Name of RData file containing the plots to output [default %default]" ),
+  make_option("--x_variable", type="character", default='FE',
+              help="Name of column to plot on the x axis [default %default]" ),
+  make_option("--fill_variable", type="character", default='Set',
+              help="Name of column to plot as the bar fill colour [default %default]" ),
+  make_option("--up_down_variable", type="character", default='up_down',
+              help="Name of column to use to determine direction of bars [default %default]" ),
+  make_option("--up_down_levels", type="character", default='Down=left,Up=right',
+              help="Levels of the up down column with directions [default %default]" ),
+  make_option("--width", type="integer", default=10,
+              help="Width of the plot [default %default]" ),
+  make_option("--height", type="integer", default=7,
+              help="Height of the plot [default %default]" ),
   make_option("--top_terms", type="integer", default=NULL,
-              help="How many terms to plot (ordered by Fold Enrichment) [default All terms]" ),
+              help="How many terms to plot (ordered by x variable) [default All terms]" ),
+  make_option("--RData_file", type="character", default=NULL,
+              help="Name of RData file containing the plots to output [default %default]" ),
   make_option("--debug", type="logical", default=FALSE, action="store_true",
               help="Turns on debugging statements [default %default]" )
 )
@@ -21,29 +35,28 @@ cmd_line_args <- parse_args(
     option_list=option_list, prog = 'go_barchart.R',
     usage = "Usage: %prog [options] input_files",
     description = desc ),
-  positional_arguments = c(1, Inf)
+  positional_arguments = 1
 )
 
 # cmd_line_args <-
 #   list(options = list('output_file' = 'go_plot.pdf',
-#                       'RData_file' = 'go_plot.rda',
+#                       'x_variable' = 'FE',
+#                       'fill_variable' = 'Set',
+#                       'up_down_variable' = 'up_down',
+#                       'up_down_levels' = 'Down=left,Up=right',
 #                       'top_terms' = NULL,
+#                       'RData_file' = 'go_plot.rda',
 #                       debug = TRUE),
-#        args = c('BP.sig.tsv', 'CC.sig.tsv', 'MF.sig.tsv'))
+#        args = c('test_data_go.tsv'))
 
 # load packages
-packages <- c('tidyverse', 'biovisr', 'grid', 'extrafont')
+packages <- c('tidyverse', 'rlang', 'biovisr', 'miscr', 'grid', 'extrafont')
 for( package in packages ){
   suppressPackageStartupMessages( suppressWarnings( library(package, character.only = TRUE) ) )
 }
 
 # load data
 go_info <- read_tsv(cmd_line_args$args[1])
-# make column, combo of Response_Category and Term_ID
-go_info <- go_info %>% 
-  mutate(., UID = paste(Response_Category, Term_ID, sep ="-"),
-         Response_Category = factor(Response_Category, levels = unique(Response_Category))) %>% 
-  arrange(., UID)
 
 # If top_terms is NULL make it equal to the number of terms
 if (is.null(cmd_line_args$options[['top_terms']])) {
@@ -52,72 +65,59 @@ if (is.null(cmd_line_args$options[['top_terms']])) {
   num_top_terms <- cmd_line_args$options[['top_terms']]
 }
 
-run_fishers_test <- function(line) {
-  in_term_in_set <- line$Num_genes_in_term_in_set[1]
-  in_term_not_set <- line$Num_genes_in_term[1] - in_term_in_set
-  not_term_in_set <- line$Study_set_size[1] - in_term_in_set
-  not_term_not_set <- line$Reference_set_size[1] - in_term_in_set - in_term_not_set - not_term_in_set
-  fisher.test(cbind(c(in_term_in_set, in_term_not_set), c(not_term_in_set, not_term_not_set)),
-              alternative = "greater")
+# go through options
+up_down_levels_l <- strsplit(unlist(strsplit(cmd_line_args$options[['up_down_levels']], ",")), "=")
+x_var <- rlang::sym(cmd_line_args$options[['x_variable']])
+up_down_var <- rlang::sym(cmd_line_args$options[['up_down_variable']])
+fill_var <- rlang::sym(cmd_line_args$options[['fill_variable']])
+
+for (up_down_level in up_down_levels_l) {
+  if (up_down_level[2] == 'left') {
+    go_info <- go_info %>% 
+      mutate(., !!x_var := case_when(!!up_down_var == up_down_level[1] ~ -!!x_var,
+                               TRUE ~ !!x_var))
+  }
 }
 
-fishers_results <- split(go_info, go_info$UID) %>% 
-  lapply(., run_fishers_test) %>% 
-  sapply(., function(x){ signif(x$p.value, digits = 6) })
-
-if (any(signif(go_info$pvalue, digits = 6) != fishers_results)) {
-  ids <- go_info$UID[ (signif(go_info$pvalue, digits = 6) != fishers_results) ]
-  go_info[ (signif(go_info$pvalue, digits = 6) != fishers_results), ] %>% 
-    cbind(., fisher_results = fishers_results[ids]) %>% 
-    select(., UID, pvalue, fisher_results, adjp, Term_name) %>% 
-    mutate(., diff = pvalue - fisher_results) %>% 
-    print()
-  warning("Some of the Pvalues don't match!")
-}
-
-go_info$FE <- (go_info$Num_genes_in_term_in_set / go_info$Study_set_size) /
-  (go_info$Num_genes_in_term/go_info$Reference_set_size)
-# order by fold enrichment and filter to top terms
-go_info_top <- go_info %>% 
-  arrange(., FE) %>% 
+# arrange by absolute FE/pval
+# take the top n terms
+# rearrange by FE/pval
+# and make GO.ID, up_down_var and fill_var into factors
+go_info <- go_info %>% 
+  arrange(., desc(abs(!!x_var))) %>% 
   head(., num_top_terms) %>% 
-  mutate(Term_name = factor(Term_name, levels = unique(Term_name)),
-         cluster = factor(cluster))
+  arrange(., !!x_var) %>% 
+  mutate(GO.ID = factor(GO.ID, levels = unique(GO.ID)),
+         up_down = factor(!!up_down_var),
+         !!fill_var := factor(!!fill_var))
 
-# output plots to pdf
-pdf(file = cmd_line_args$options[['output_file']],
-    width = 10)
-# Coloured bar chart
-# change FE to -ve if cluster2
-go_info_top <- go_info_top %>% 
-  mutate(., FE = ifelse(cluster == 'Cluster1', FE, -FE)) %>% 
-  arrange(., FE) %>% 
-  mutate(Term_name = factor(Term_name, levels = unique(Term_name)))
-
+# Text for labelling the x axis
 up_label <- grobTree(textGrob(label = "Up genes", gp = gpar(fontsize = 10, fontfamily = "Arial")))
 down_label <- grobTree(textGrob(label = "Down genes", gp = gpar(fontsize = 10, fontfamily = "Arial")))
 
-# version with labels next to bars
-up_term_labels <- go_info_top %>% 
-  filter(., FE > 0) %>% 
-  select(., Term_name) %>% 
+# vectors for labelling the bars
+up_term_labels <- go_info %>% 
+  filter(., !!x_var > 0) %>% 
+  select(., GO.ID, Term) %>% 
   unique(.) %>% 
   mutate(., y = -1)
-down_term_labels <- go_info_top %>% 
-  filter(., FE < 0) %>% 
-  select(., Term_name) %>% 
+down_term_labels <- go_info %>% 
+  filter(., !!x_var < 0) %>% 
+  select(., GO.ID, Term) %>% 
   unique(.) %>% 
   mutate(., y = 1)
-coloured_bar_chart <- ggplot(data = go_info_top) +
-  geom_col(aes(x = Term_name, y = FE, fill = Response_Category),
+
+# make bar chart
+coloured_bar_chart <- ggplot(data = go_info) +
+  geom_col(aes(x = GO.ID, y = !!x_var, fill = !!fill_var),
            position = "dodge") +
   geom_hline(yintercept = 0, linetype = "dashed") +
   geom_text(data = up_term_labels, hjust = 1, vjust = 0.5, size = 10/72*25.4,
-            aes(x = Term_name, y = y, label = Term_name)) +
+            aes(x = GO.ID, y = y, label = Term)) +
   geom_text(data = down_term_labels, hjust = 0, vjust = 0.5, size = 10/72*25.4,
-            aes(x = Term_name, y = y, label = Term_name)) +
-  scale_fill_manual(values = cbf_palette(nlevels(go_info_top$Response_Category)),
-                    ) +
+            aes(x = GO.ID, y = y, label = Term)) +
+  scale_fill_manual(values = cbf_palette(nlevels(go_info[[ cmd_line_args$options[['fill_variable']] ]]))) +
+  scale_y_continuous(labels = abs) +
   coord_flip(clip = "off") +
   annotation_custom(up_label,
                     xmin = 0, xmax = -2,
@@ -133,13 +133,22 @@ coloured_bar_chart <- ggplot(data = go_info_top) +
         axis.ticks.y = element_blank(),
         axis.title.x = element_text(margin = margin(20,0,0,0)),
         panel.grid.major.y = element_blank())
-print(coloured_bar_chart)
 
+# output plot
+miscr::open_graphics_device(cmd_line_args$options[['output_file']],
+                            width = cmd_line_args$options[['width']],
+                            height = cmd_line_args$options[['height']])
+print(coloured_bar_chart)
 dev.off()
 
-# embed fonts
-embed_fonts(cmd_line_args$options[['output_file']], 
-            outfile = cmd_line_args$options[['output_file']])
+# embed fonts if it's a pdf
+if (grepl("pdf$", cmd_line_args$options[['output_file']])) {
+  embed_fonts(cmd_line_args$options[['output_file']], 
+              outfile = cmd_line_args$options[['output_file']])
+}
 
-save(facetted_barchart, coloured_bar_chart, go_info_top,
-     file = cmd_line_args$options[['RData_file']])
+# save to rdata file if option specified
+if (!is.null(cmd_line_args$options[['RData_file']])) {
+  save(coloured_bar_chart, go_info,
+       file = cmd_line_args$options[['RData_file']])
+}
