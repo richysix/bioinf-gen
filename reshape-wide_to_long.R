3dfe3231a7739a646aaa712658fab200870e2245
Richard White <rjw26@cam.ac.uk>
2021-01-28 22:13:04 +0000

    reshape-wide_to_long.R: Create script to reshape data
    Takes a file and makes it longer. It currently pivots every
    column except once named sample.

---
+++ reshape-wide_to_long.R
@@ -0,0 +1,32 @@
+#!/usr/bin/env Rscript
+
+library('optparse')
+
+option_list <- list(
+  make_option("--debug", type="logical", default=FALSE, action="store_true",
+            help="Turns on debugging statements [default %default]" )
+)
+
+desc <- paste('', 'Script to reformat a file from wide to long data',
+    'Currently assumes that everything but the sample column is to be pivoted.',
+    sep = "\n")
+
+cmd_line_args <- parse_args(
+  OptionParser(
+    option_list=option_list, prog = 'reshape-wide_to_long.R',
+    usage = "Usage: %prog [options] inputFile", description = desc ),
+  positional_arguments = 2
+)
+
+library('tidyverse')
+
+input_file <- cmd_line_args$args[1]
+if (input_file == '-') {
+    input_file <- file("stdin")
+}
+metadata <- read_tsv(input_file)
+
+metadata <- metadata %>%
+    pivot_longer(., cols = -sample, names_to = "category", values_to = "value")
+
+write_tsv(metadata, cmd_line_args$args[2])
