c11330cf5432c93ddae97ad78d71580404d869f7
Richard White <rjw26@cam.ac.uk>
2021-05-17 15:49:10 +0100

    karyotype.R: Add heatmap data and colours

--- karyotype.R
+++ karyotype.R
@@ -5,6 +5,8 @@ option_list <- list(
               help="Output file name [default %default]" ),
   make_option("--heatmap_data", type="character", default=NULL,
               help="tsv of data for chromosome heatmap. Expects columns Chr, Start, End, Value [default %default]" ),
+  make_option("--heatmap_colours", type="character", default=NULL,
+              help="comma separated string of colours [default %default]" ),
   make_option("--annotation_file", type="character", default='mutation.txt',
               help="tsv of annotation [default %default]" ),
   make_option("--width", type="integer", default=NULL,
@@ -30,9 +32,10 @@ cmd_line_args <- parse_args(
 
 # cmd_line_args <-
 #   list(options = list('output_file' = 'ideogram.png',
+#                       'heatmap_data' = 'binned_sig_genes.bed',
 #                       debug = TRUE),
 #        args = c('~/citiid/hpc-work/resources/genomes/danio_rerio/grcz11/GRCz11.bed'))
-
+ 
 # load packages
 packages <- c('tidyverse', 'RIdeogram')
 for( package in packages ){
@@ -43,6 +46,20 @@ for( package in packages ){
 chr_sizes <- read_tsv(cmd_line_args$args[1],
                       col_names = c("Chr", "Start", "End"))
 
+# load heatmap data
+if (is.null(cmd_line_args$options[['heatmap_data']])) {
+  heatmap_data <- NULL
+} else {
+  heatmap_data <- read_tsv(cmd_line_args$options[['heatmap_data']])
+}
+# load colours
+if (is.null(cmd_line_args$options[['heatmap_colours']])) {
+  heatmap_colours <- c("#FFFFFF", "firebrick3")
+} else {
+  heatmap_colours <- str_split(cmd_line_args$options[['heatmap_colours']], ",")[[1]]
+}
+
+
 # # load annotation data
 # annotation <- read_tsv(cmd_line_args$options[['annotation_file']])
 
@@ -52,12 +69,13 @@ output_svg <- sub("\\..*$", ".svg", cmd_line_args$options[['output_file']])
 plot_width <- ifelse(is.null(cmd_line_args$options[['width']]), 8.2677, cmd_line_args$options[['width']])
 plot_height <- ifelse(is.null(cmd_line_args$options[['height']]), 11.6929, cmd_line_args$options[['height']])
 
-ideogram(karyotype = chr_sizes, output = output_svg)
+ideogram(karyotype = chr_sizes, output = output_svg,
+         overlaid = heatmap_data, colorset1 = heatmap_colours)
 convertSVG(output_svg, device = plot_device,
            file = cmd_line_args$options[['output_file']],
            width = plot_width, height = plot_height )
 
 # , 
-#          overlaid = binned_gene_counts, colorset1 = c("#FFFFFF", "firebrick3"),
+#          overlaid = binned_gene_counts, colorset1 = ,
 #          label = annotation, label_type = "marker")
 
