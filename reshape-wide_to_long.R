4d5366416d0a6ae05b80ab43b0690c46406f1f03
Richard White <rjw26@cam.ac.uk>
2021-06-03 11:24:30 +0100

    Add --columns option

--- reshape-wide_to_long.R
+++ reshape-wide_to_long.R
@@ -4,11 +4,16 @@ library('optparse')
 
 option_list <- list(
   make_option("--debug", type="logical", default=FALSE, action="store_true",
-            help="Turns on debugging statements [default %default]" )
+            help="Turns on debugging statements [default %default]" ),
+  make_option("--columns", type="character", default=NULL,
+              help="Name of variables to pivot [default All but the first column]" ),
+  make_option("--variable.name", type="character", default='variable',
+              help="Name of variable for measured variable names [default %default]" ),
+  make_option("--value.name", type="character", default='value',
+              help="Name of variable used to store values [default %default]" )
 )
 
 desc <- paste('', 'Script to reformat a file from wide to long data',
-    'Currently assumes that everything but the sample column is to be pivoted.',
     sep = "\n")
 
 cmd_line_args <- parse_args(
@@ -24,12 +29,34 @@ input_file <- cmd_line_args$args[1]
 if (input_file == '-') {
     input_file <- file("stdin")
 }
-metadata <- read_tsv(input_file)
+data <- read_tsv(input_file)
 
-metadata <- metadata %>%
-  pivot_longer(., cols = -sample, names_to = "category", values_to = "value") %>% 
-  mutate(., category = factor(category, levels = unique(category))) %>% 
-  arrange(., category, value) %>% 
-  mutate(., value = factor(value, levels = unique(value)))
+# ID variables
+if (is.null(cmd_line_args$options[['columns']])) {
+  col_ids <- setdiff(seq_len(ncol(data)), c(1))
+} else {
+  col_names <- unlist(strsplit(cmd_line_args$options[['columns']], ','))
+  # check variable names
+  col_names_exist <- col_names %in% names(data)
+  if (any(!col_names_exist)) {
+    missing_cols <- col_names[ !col_names_exist ]
+    err_msg <- paste0('At least one of the --columns names are missing from the data\n',
+                      'Missing: ', paste0(missing_cols, collapse = ', '), '\n')
+    stop(err_msg)
+  }
+  col_ids <- which(colnames(data) %in% col_names)
+}
 
-write_tsv(metadata, cmd_line_args$args[2])
+data <- data %>%
+  pivot_longer(., cols = all_of(col_ids), 
+               names_to = cmd_line_args$options[["variable.name"]], 
+               values_to = cmd_line_args$options[["value.name"]])
+  # mutate(., category = factor(category, levels = unique(category))) %>% 
+  # arrange(., category, value) %>% 
+  # mutate(., value = factor(value, levels = unique(value)))
+
+output_file <- cmd_line_args$args[2]
+# if (output_file == '-') {
+#   output_file <- file("stdout")
+# }
+write_tsv(data, output_file)
