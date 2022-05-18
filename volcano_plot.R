ad3c3ac47d387d39075af9bddd5e96b17214968e
Richard White <richard.white@qmul.ac.uk>
2022-05-18 12:03:03 +0100

    Update volcano_plot.R
    Check whether the ggrastr package is installed and use geom_point if not
    and check for svglite package and use svg function instead if not installed
    Update loading data to tidyverse

--- volcano_plot.R
+++ volcano_plot.R
@@ -12,7 +12,7 @@ option_list <- list(
               help="P value cut-off to use to colour/label the points [default %default]" ),
   make_option("--log2fc_or_pval", action="store_true", type="logical", default=FALSE,
               help=paste0("Change labelling to be points over either threshold [default %default]\n",
-              "The default behaviour if both thresholds are set is to labels points over both thresholds\n") )
+                          "The default behaviour if both thresholds are set is to labels points over both thresholds\n") )
 )
 
 cmd_line_args <- parse_args(
@@ -23,68 +23,56 @@ cmd_line_args <- parse_args(
   positional_arguments = 2
 )
 
-# cmd_line_args <-
-#    list(options = list(),
-#        args = c('deseq2/3dpf_uninf_hom_vs_3dpf_uninf_sib.tsv',
-#                 'deseq2/3dpf_uninf_hom_vs_3dpf_uninf_sib.volcano.pdf'))
-
 # unpack options
 deseq_results_file <- cmd_line_args$args[1]
 output_file <- cmd_line_args$args[2]
 
 # load packages 
-packages <- c('tidyverse', 'viridis', 'ggrepel', 'ggrastr')
-if (grepl('svg$', output_file)) { packages <- c(packages, 'svglite') }
+packages <- c('tidyverse', 'ggrepel', 'miscr')
 for( package in packages ){
-    suppressPackageStartupMessages(
-        suppressWarnings( library(package, character.only = TRUE) ) )
+  suppressPackageStartupMessages(
+    suppressWarnings( library(package, character.only = TRUE) ) )
 }
 
 if (!is.null(cmd_line_args$options[['log2fc_threshold']]) |
-      !is.null(cmd_line_args$options[['pval_threshold']])) {
-    cmd_line_args$options[['labels']] <- TRUE
+    !is.null(cmd_line_args$options[['pval_threshold']])) {
+  cmd_line_args$options[['labels']] <- TRUE
 }
 
 # load data
 deseq_results <- read_tsv(deseq_results_file) %>% 
-  filter(., !is.na(adjp)) # remove gene with adjp == NA
-
-# make -log10p column
-deseq_results$log10p <- -log10(deseq_results$adjp)
-
-# make factor for up and down genes
-deseq_results$sig <- deseq_results$adjp < 0.05
-deseq_results$up_or_down <- rep(NA, nrow(deseq_results))
-deseq_results$up_or_down[ deseq_results$sig & deseq_results$log2fc > 0 ] <- 'up'
-deseq_results$up_or_down[ deseq_results$sig & deseq_results$log2fc < 0 ] <- 'down'
-deseq_results$up_or_down <- factor(deseq_results$up_or_down,
-                                   levels = c('up', 'down'))
-
-# sort by Adjusted pvalue
-deseq_results <- arrange(deseq_results, desc(adjp))
+  filter(., !is.na(adjp)) %>% # remove gene with adjp == NA
+  mutate(., log10p = -log10(adjp),
+         up_or_down = factor(case_when( # make factor for up and down genes
+           adjp < 0.05 & log2fc > 0 ~ 'up',
+           adjp < 0.05 & log2fc < 0 ~ 'down'
+         ), levels = c('up', 'down'))
+  ) %>% 
+  arrange(., desc(adjp)) # sort by Adjusted pvalue
 
 # highlight specific genes
-# label genes with abs(fold change) > 2
 if (cmd_line_args$options[['labels']]) {
   # set defaults if not set
   if (is.null(cmd_line_args$options[['log2fc_threshold']]) &
       is.null(cmd_line_args$options[['pval_threshold']])) {
+    # abs(fold change) > 2
     cmd_line_args$options[['log2fc_threshold']] <- 2
+    # adjp < 1e-5
     cmd_line_args$options[['pval_threshold']] <- 1e-5
   }
   deseq_results$name_label <- ""
   if (!is.null(cmd_line_args$options[['log2fc_threshold']])) {
-      if (!is.null(cmd_line_args$options[['pval_threshold']])) {
-        if (cmd_line_args$options[['log2fc_or_pval']]) {
-            to_label <- abs(deseq_results$log2fc) > cmd_line_args$options[['log2fc_threshold']] |
-                            deseq_results$adjp < cmd_line_args$options[['pval_threshold']]
-        } else {
-            to_label <- abs(deseq_results$log2fc) > cmd_line_args$options[['log2fc_threshold']] &
-                            deseq_results$adjp < cmd_line_args$options[['pval_threshold']]
-        }
+    if (!is.null(cmd_line_args$options[['pval_threshold']])) {
+      if (cmd_line_args$options[['log2fc_or_pval']]) {
+        to_label <- abs(deseq_results$log2fc) > cmd_line_args$options[['log2fc_threshold']] |
+          deseq_results$adjp < cmd_line_args$options[['pval_threshold']]
       } else {
-        to_label <- abs(deseq_results$log2fc) > cmd_line_args$options[['log2fc_threshold']]
+        to_label <- abs(deseq_results$log2fc) > cmd_line_args$options[['log2fc_threshold']] &
+          deseq_results$adjp < cmd_line_args$options[['pval_threshold']]
       }
+    } else {
+      to_label <- abs(deseq_results$log2fc) > cmd_line_args$options[['log2fc_threshold']]
+    }
   } else {
     to_label <- deseq_results$adjp < cmd_line_args$options[['pval_threshold']]
   }
@@ -93,16 +81,29 @@ if (cmd_line_args$options[['labels']]) {
 
 # plot adjusted pvalue against log2 fold change
 # with up coloured in orange and down coloured in blue
-# label highest changers
+# label genes above the pvalue/log2fc thresholds
 volcano_plot <-
-    ggplot(data = deseq_results, aes(x = log2fc, y = log10p, colour = up_or_down)) +
-        #geom_point()
-        rasterise(geom_point(), dpi = 300)
-if (cmd_line_args$options[['labels']]) {
+  ggplot(data = deseq_results, aes(x = log2fc, y = log10p, colour = up_or_down))
+
+# Use ggrastr if it is installed to make final image smaller
+if(suppressPackageStartupMessages(suppressWarnings(require('ggrastr')))) {
   volcano_plot <- volcano_plot +
-        geom_text_repel(aes(label = name_label)) 
+    rasterise(geom_point(), dpi = 300)
+} else { # otherwise use geom_point
+  cat("Package ggrastr not available. Continuing without it.\n")
+  volcano_plot <- volcano_plot +
+    geom_point()
 }
 
+# Add gene name labels
+if (cmd_line_args$options[['labels']]) {
+  volcano_plot <- volcano_plot +
+    geom_text_repel(aes(label = name_label),
+                    point.padding = 0.25, box.padding = 1,
+                    force = 2)
+}
+
+# Add colour scale, chnge theme and add axis labels
 volcano_plot <- volcano_plot +
   scale_colour_manual(values = c(up = '#cc6600', down = '#0073b3'), na.value = "grey80") +
   theme_minimal() +
@@ -116,12 +117,14 @@ volcano_plot <- volcano_plot +
 if (sub("^.*\\.", "", output_file) == "eps") {
   postscript(file = output_file)
 } else if (sub("^.*\\.", "", output_file) == "svg") {
-  library('svglite')
-  svglite(file = output_file)
+  if(suppressPackageStartupMessages(suppressWarnings(require('svglite')))) {
+    svglite(file = output_file)
+  } else {
+    svg(filename = output_file)
+  }
 } else {
   pdf(file = output_file)
 }
-
 # print plot and close graphics device
 print(volcano_plot)
 dev.off()
