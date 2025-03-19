69cfe92e14b871553725a75cc9ee25448f8eee93
Richard White <richard.white@qmul.ac.uk>
2025-03-19 17:11:17 +0000

    Update karyotype.R
    Add default input file for interactive use

--- karyotype.R
+++ karyotype.R
@@ -1,7 +1,7 @@
 library('optparse')
 
 option_list <- list(
-  make_option("--output_file", type="character", default='ideogram.png',
+  make_option("--output_file", type="character", default = 'ideogram.png',
               help="Output file name [default %default]" ),
   make_option("--heatmap_data", type="character", default=NULL,
               help="tsv of data for chromosome heatmap. Expects columns Chr, Start, End, Value [default %default]" ),
@@ -24,20 +24,21 @@ desc <- paste(
   sep = "\n"
 )
 
+if (interactive()) {
+  cmd_args <- c("/Users/rjw26/work/apocrita/data/scratch/bty114/zmp-network/Dr.e85.chrom.sizes")
+} else {
+  cmd_args <- commandArgs(trailingOnly = TRUE)
+}
+
 cmd_line_args <- parse_args(
   OptionParser(
-    option_list=option_list, prog = 'karyotype.R',
-    usage = "Usage: %prog [options] chr.sizes",
-    description = desc ),
+    option_list = option_list, prog = 'karyotype.R',
+    description = desc,
+    usage = "Usage: %prog [options] chr.sizes"),
+  args = cmd_args,
   positional_arguments = 1
 )
 
-# cmd_line_args <-
-#   list(options = list('output_file' = 'ideogram.png',
-#                       'heatmap_data' = 'binned_sig_genes.bed',
-#                       debug = TRUE),
-#        args = c('~/citiid/hpc-work/resources/genomes/danio_rerio/grcz11/GRCz11.bed'))
- 
 # load packages
 packages <- c('tidyverse', 'RIdeogram', 'biovisr')
 for( package in packages ){
@@ -75,7 +76,7 @@ plot_width <- ifelse(is.null(cmd_line_args$options[['width']]), 8.2677, cmd_line
 plot_height <- ifelse(is.null(cmd_line_args$options[['height']]), 11.6929, cmd_line_args$options[['height']])
 
 ideogram(karyotype = chr_sizes, output = output_svg,
-         overlaid = heatmap_data, colorset1 = heatmap_colours,
+         # overlaid = heatmap_data, colorset1 = heatmap_colours,
          label = annotation_data, label_type = cmd_line_args$options[['annotation_label_type']])
 convertSVG(output_svg, device = plot_device,
            file = cmd_line_args$options[['output_file']],
